e6c155edac820f527a5f86b57c12b7ff379574c4
tashi-sk <tashi_sk@hotmail.com>
2021-07-27 16:44:09 +0000

    removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', false)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
